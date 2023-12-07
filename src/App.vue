c75a8242f8e783f9e8fa4cd0ff0dba3979e14502
liminghui <3209498857@qq.com>
2023-12-07 18:25:15 +0800

    useDark

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <script setup>
-import { useMouse } from '@vueuse/core'
-const { x, y } = useMouse()
-const mouse = useMouse()
+import { useDark, useToggle, useTitle } from '@vueuse/core'
+const dark = useDark({
+  // 这里是选择要变色的容器
+  selector: 'html'
+})
+const toggleDark = useToggle(dark)
+// 需要把.dark {background-color: #ccc;}的scoped 去掉
+// 可以修改标题
+useTitle(() => dark.value ? '🌙 Good evening!' : '☀️ Good morning!')
 </script>
 
 <template>
-  <div>
+  <div color-scheme="dark">
     vueuse
-    <ul>
-      <li>x:{{ x }}</li>
-      <li>y:{{ y }}</li>
-      <li>mouse:{{ mouse }}</li>
-    </ul>
   </div>
+  {{ dark ? '🌙 Good evening!' : '☀️ Good morning!' }}
+  <button @click="toggleDark()">转变</button>
 </template>
 
-<style scoped></style>
+<style>
+div {
+  border: 1px solid #000;
+  width: 200px;
+  height: 300px;
+}
+
+html.dark {
+  background-color: #ccc;
+}
+</style>
