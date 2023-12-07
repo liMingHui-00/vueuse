c71b0ddf17361b28c29c55e94deb303135d8d28d
liminghui <3209498857@qq.com>
2023-12-07 17:45:25 +0800

    useMouse

---
+++ src/useMouse.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useMouse } from '@vueuse/core'
+const { x, y } = useMouse()
+const mouse = useMouse()
+</script>
+
+<template>
+  <div>
+    vueuse
+    <ul>
+      <li>x:{{ x }}</li>
+      <li>y:{{ y }}</li>
+      <li>mouse:{{ mouse }}</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
